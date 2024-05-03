4199c5dd78224ca9a3b986845a5f106ea69aae71
Dido2002 <didoguzelev@gmail.com>
2024-05-03 12:05:31 +0300

    Working on register design and functionality

--- app/src/main/java/com/example/healthcare/LoginActivity.java
+++ app/src/main/java/com/example/healthcare/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.healthcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -35,5 +36,12 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+            }
+        });
+
     }
 }
